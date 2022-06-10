4daecf19ce4198d8f1bb31b09490bd4e087a745f
Facundo Rojas <facuroja@hotmail.com>
2022-06-10 14:24:57 -0300

    Clase 2 Variables en Python

---
+++ Leccion1/main.py
@@ -0,0 +1,18 @@
+print('Hola Mundo')
+
+miVariable = 3
+print(miVariable)
+miVariable = "Hola a todo aquel que lea"
+print(miVariable)
+miVariable = 3.5
+print(miVariable)
+x = 10
+y = 2
+z = x + y
+print(id(x))
+# Las literales se escriben con x752, los ultimos 3 numeros de su id
+print(id(y))
+print(id(z))
+# Ejemplo Variable x = x752 / Variable y = x368 / Variable z = x688
+# esas serian las literales de cada variable
+# a esto se lo conoce como referencia de memoria
