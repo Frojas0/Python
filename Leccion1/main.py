0fe7a043603fbfd254932913553782b35fbc5f44
Facundo Rojas <facuroja@hotmail.com>
2022-06-16 18:21:01 -0300

    Clase 4 Operadores en Python Parte 1

--- Leccion1/main.py
+++ Leccion1/main.py
@@ -1,3 +1,4 @@
+'''
 print('Hola Mundo')
 
 # Clase 2
@@ -75,4 +76,104 @@ print("Mi dia estuvo de: ", respuesta)
 titulo = input("Proporciona el titulo: ")
 autor = input ("Proporciona el autor: ")
 print(titulo, "fue escrito por", autor)
+'''  # Triple comilla para comentario extenso
 
+"""
+# Operadores aritmeticos
+operandoA = 8
+operandoB = 5
+suma = operandoA + operandoB
+#print("Resultado de la suma: ", suma)
+print(f'El resultado de la suma es: {suma}') # Imprimiendo con Format (INTERPOLACION)
+
+resta = operandoA - operandoB
+print(f'El resultado de la resta es: {resta}')
+
+multiplicacion = operandoA * operandoB
+print(f'EL resultado de la multiplicacion es: {multiplicacion}')
+
+division = operandoA / operandoB
+print(f'El resultado de la division es: {division}')
+division = operandoA //operandoB # TRUNCADO
+print(f'El resultado de la division es: {division}')
+
+modulo = operandoA % operandoB
+print(f'El resultado del modulo es: {modulo}')
+
+exponente = operandoA ** operandoB
+print(f'El resultado del exponente es: {exponente}')
+
+a = int(input('Escribe el valor de a'))
+"""
+"""
+# EJERCICIO: Calcular area y perimetro de un rectangulo
+
+alto = int(input("Ingrese la altura del Rectangulo: "))
+ancho = int(input("Ingrese el ancho del Rectangulo: "))
+area = alto * ancho
+perimetro = (alto + ancho) * 2
+print(f'''
+Altura proporcionada: {alto}
+Ancho proporcionado: {ancho}
+Area del rectangulo: {area}
+Perimetro del rectangulo: {perimetro}
+''')
+"""
+"""
+miVariable3 = 10
+print(miVariable3)
+
+# Operadores de reasignacion
+
+miVariable3 = miVariable3 + 1
+print(miVariable3)
+
+miVariable3 +=  1 # Incremento
+print(miVariable3)
+
+miVariable3 -= 2 # Decremento
+print(miVariable3)
+
+miVariable3 *= 3 # Producto
+print(miVariable3)
+
+miVariable3 /= 2 # Division
+print(miVariable3)
+
+# Operadores de comparacion
+
+d = 4
+b = 2
+resultado = d == b # Comprobamos igualdad
+print(resultado)
+
+resultado = d != b # Comprobamos desigualdad
+print(resultado)
+
+resultado = d > b # Operador mayor que
+print(resultado)
+
+resultado = d < b # Operador menor que
+print(resultado)
+
+resultado = d <= b # Operador menor o igual que
+print(resultado)
+
+resultado = d >= b # Operador mayor o igual que
+print(resultado)
+"""
+"""
+# EJERCICIO 1: Numero par o impar
+variable = int(input("Ingrese un numero: "))
+if variable % 2 == 0:
+    print(f"El numero {variable} es PAR")
+else:
+    print(f"El numero {variable} es IMPAR")
+
+# EJERCICIO 2: Determinar si es mayor de edad
+edad = int(input("Ingrese su edad"))
+if edad >= 18:
+    print(f"Tu edad es: {edad}años, eres mayor de edad")
+else:
+    print(f"Tu edad es: {edad}años, eres menor de edad")
+"""
