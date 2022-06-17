d936eca0b84e1332d9e2cd25d35c5d4d454926ec
Facundo Rojas <facuroja@hotmail.com>
2022-06-16 23:26:01 -0300

    Clase 5 Operadores en Python Parte 2

--- Leccion1/main.py
+++ Leccion1/main.py
@@ -177,3 +177,76 @@ if edad >= 18:
 else:
     print(f"Tu edad es: {edad}años, eres menor de edad")
 """
+"""
+# Operadores Logicos
+
+a = False
+b= True
+resultado = a and b # and (operador binario)
+print(resultado)
+
+resultado = a or b # or (operador binario)
+print(resultado)
+
+resultado = not a # not (operador unario)
+print(resultado)
+"""
+
+"""
+# EJERCICIO: Valor dentro de un rango
+
+valor = int(input("Ingrese un numero: "))
+if 0 <= valor <= 5: # es lo mismo que decir (valor >= 0 and valor <=5)
+    print(f"El valor {valor} esta dentro del rango")
+else:
+    print(f"El valor {valor} no esta dentro del rango")
+"""
+"""
+# EJERCICIO: Operador or
+vacaciones = True
+diaLibre = False
+if vacaciones or diaLibre:
+    print("Puede asistir al juego")
+else:
+    print("Tiene trabajo que hacer")
+"""
+"""
+# EJERCICIO: Rango entre edades
+edad = int(input("Digite su edad"))
+
+# se deben respetar los tabulados en bloques de codigo (identacion)
+if (20<= edad <30) or (30<= edad <40):
+    print(f"Tienes {edad} Estas en el rango de 20'30")
+else:
+    print(f"Tienes {edad} estas fuera del rango de los 20/30")
+"""
+"""
+# EJERCICIO1: El mayor de dos numeros
+num1 = int(input("Digite el valor para el numero1: "))
+num2 = int(input("Digite el valor para el numero2: "))
+if num1 > num2:
+    print(f"El numero mayor es: {num1}")
+else:
+    print(f"El numero mayor es: {num2}")
+"""
+"""
+# EJERCICIO: Tienda de libros
+nombre = input("Digite el nombre del libro: ")
+id = input("Digite el ID del libro: ")
+precio = float(input("Digite el precio del libro: "))
+envio = input("Envio gratis? (True/False): ")
+
+if envio == "True":
+    envio = "El envio es Gratuito"
+elif envio == "False":
+    envio = "El envio No es gratuito"
+else:
+    envio = "Valor incorrecto, ingrese: True/False"
+
+print(f"""
+#Nombre: {nombre}
+#ID: {id}
+#Precio: ${precio}
+#{envio}
+""")
+"""
